96531b5c7ef6b69894d2254a4f9e9f8a4c83cbdc
Christoph Bayer <chrbayer@criby.de>
2021-07-16 11:02:10 +0200

    Enhance kickc example

--- kickc/soc-test.c
+++ kickc/soc-test.c
@@ -3,17 +3,24 @@
 #include "soc.h"
 
 
+const unsigned int LOW = 0x1000;
+const unsigned int HIGH = 0xF000;
+const unsigned int INC = 0x100;
+
+unsigned int t = 0x4000;
+bool d = true;
 unsigned char x = 0;
 
 
 void main() {
+    unsigned char old = 0;
 
     // Configure CIA
     CIA->PORT_B_DDR = 0xC0;
-    CIA->TIMER_B = 0x4000;
 
     // Infinite loop
     while(1) {
+        CIA->TIMER_B = t;
         CIA->TIMER_B_CONTROL = CIA_TIMER_CONTROL_ONESHOT | CIA_TIMER_CONTROL_START;
 
         while (CIA->TIMER_B_CONTROL & CIA_TIMER_CONTROL_START)
@@ -23,6 +30,23 @@ void main() {
         // Write counter to port
         CIA->PORT_B = (x & 0xC0);
         x++;
+
+        if ((x & 0xC0) != (old & 0xC0)) {
+            if (d) {
+                t += INC;
+                if (t > HIGH) {
+                    d = false;
+                }
+            }
+            else {
+                t -= INC;
+                if (t < LOW) {
+                    d = true;
+                }
+            }
+        }
+
+        old = x;
     }
 }
 
